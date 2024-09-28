a7b7be08351557ce136344d8eb80676e29ffad3c
adebanjoemmanuel01 <adebanjoemmanuel01@gmail.com>
2024-09-28 17:21:40 +0100

    fixed: set the prod db to use the appropriate dbConfig

--- src/libs/database/config/database.config.ts
+++ src/libs/database/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () =>
-  process.env.NODE_ENV == 'development' ? remoteDBConfig(): localDBConfig(),
+  process.env.NODE_ENV == 'development' ? localDBConfig() : remoteDBConfig(),
 );
 
 const localDBConfig = () => {
