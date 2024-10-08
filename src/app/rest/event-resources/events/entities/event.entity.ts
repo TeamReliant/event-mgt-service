179a56ab384c233a4816bd7494e4d823cb4e8616
adebanjoemmanuel01 <adebanjoemmanuel01@gmail.com>
2024-10-08 17:21:46 +0100

    refactored event entity to contain more information for event

--- src/app/rest/event-resources/events/entities/event.entity.ts
+++ src/app/rest/event-resources/events/entities/event.entity.ts
@@ -21,11 +21,17 @@ export class Event extends AbstractEntity<Event> {
   @Column()
   locationName: string;
 
+  @Column({ nullable: true })
+  googleMapUrl: string;
+
   @Column()
   address: string;
 
   @Column({ nullable: true })
   description: string;
+
+  @Column({ nullable: true })
+  additionalInfo: string;
 
   @Column({ nullable: true })
   eventImageURL: string;
@@ -47,17 +53,14 @@ export class Event extends AbstractEntity<Event> {
   })
   eventStatus: EventStatus;
 
-  @Column()
-  eventStartDate: Date;
+  @Column('timestamp', { nullable: true })
+  eventStartDateAndTime: Date;
+
+  @Column('timestamp', { nullable: true })
+  eventEndDateAndTime: Date;
 
   @Column({ nullable: true })
-  eventEndDate: Date;
-
-  @Column('time')
-  eventStartTime: string;
-
-  @Column('time', { nullable: true })
-  eventEndTime: string;
+  timeZone: string;
 
   // should be set to false when all ticket in tickets.isAvailable returns false
   //  should be set to false when eventEndDate is less than current date
