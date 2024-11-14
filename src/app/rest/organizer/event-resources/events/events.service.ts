81ac8d1372868c18839ee74624678247c25c0af2
Adurotimi Joshua Steven <74197839+fearless-ajs@users.noreply.github.com>
2024-11-14 20:39:45 +0100

    Update events.service.ts

--- src/app/rest/organizer/event-resources/events/events.service.ts
+++ src/app/rest/organizer/event-resources/events/events.service.ts
@@ -442,6 +442,52 @@ export class EventsService {
     return updatedEvent;
   }
 
+
+    /**
+   * A method to find all events in the database based on some query parameters
+   * @param params this is an object containing key value pairs of query parameters
+   * @returns the list of events
+   */
+  findAll(params?: { [key: string]: any }) {
+    const queryBuilder = this.eventRepo.createQueryBuilder('event');
+    queryBuilder.andWhere('event.eventVisibility = :publicVisibility', {
+      publicVisibility: 'public',
+    });
+    queryBuilder.andWhere('event.eventStatus = :publishedStatus', {
+      publishedStatus: 'published',
+    });
+
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        if (params[key]) {
+          if (key === 'tags') {
+            queryBuilder.andWhere(
+              `regexp_split_to_array(event.tags, '[,\\s]+') @> ARRAY[:tag]`,
+              { tag: params[key] },
+            );
+          } else if (key === 'eventStartDateAndTime') {
+            const today = new Date();
+            queryBuilder.andWhere(
+              `event.eventStartDateAndTime BETWEEN :today AND :eventEndDate`,
+              {
+                today: today.toISOString(),
+                eventEndDate: params[key],
+              },
+            );
+          } else {
+            queryBuilder.andWhere(`event.${key} ILIKE :${key}`, {
+              [key]: `%${params[key]}%`,
+            });
+          }
+        }
+      });
+    }
+
+
+    return queryBuilder.getMany();
+  }
+  
+
   async remove(id: string, user: TJwtPayload) {
     //check if event exists and belongs to authenticated user
     const event = await this.findOne(id, user);
