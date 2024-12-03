c216e6b21f6a45a08f0c748c4bd82a3242a4058c
adebanjoemmanuel01 <adebanjoemmanuel01@gmail.com>
2024-12-03 07:39:58 +0100

    Updated event dto to make location name and placeId optional

--- src/app/rest/organizer/event-resources/events/dto/create-event.dto.ts
+++ src/app/rest/organizer/event-resources/events/dto/create-event.dto.ts
@@ -30,10 +30,12 @@ export class CreateEventDto {
 
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   locationName: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsOptional()
   locationPlaceId: string;
 
   @IsOptional()
