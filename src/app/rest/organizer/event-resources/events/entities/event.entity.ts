197b53e8013c22827688861f9ffd2d1a6a5de0d9
Emmanuel Adebanjo <adebanjoemmanuel01@gmail.com>
2024-12-20 00:29:50 +0100

    Handled relationships on delete of event

--- src/app/rest/organizer/event-resources/events/entities/event.entity.ts
+++ src/app/rest/organizer/event-resources/events/entities/event.entity.ts
@@ -112,6 +112,7 @@ export class Event extends AbstractEntity<Event> {
 
   @OneToMany(() => EventView, (view) => view.event, {
     cascade: true,
+    onDelete: 'CASCADE',
   })
   eventViews?: EventView[];
 
