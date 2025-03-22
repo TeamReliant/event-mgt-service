8556463669ac97b39185b0a73570f64d71044d2a
Adurotimi Joshua Steven <adurotimijoshua@gmail.com>
2025-03-22 12:15:24 +0100

    fixed mailing

--- src/app/rest/attendee/bookings/bookings.service.ts
+++ src/app/rest/attendee/bookings/bookings.service.ts
@@ -518,8 +518,7 @@ export class BookingsService {
       await manager.save(Ticket, ticket);
     });
 
-  
-    console.log(">>>>>>>I got here<<<<<<<<<<<<<<<<<<<<<<<<<<", newBookings);
+    console.log('>>>>>>>I got here<<<<<<<<<<<<<<<<<<<<<<<<<<', newBookings);
     this._eventEmitter.emit(
       events.BOOKING_COMPLETED,
       new BookingsEvent(newBookings),
