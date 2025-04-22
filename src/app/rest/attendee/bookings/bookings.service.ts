6a89e87b482b716e35bfc9d7813354c447e5a114
Adurotimi Joshua Steven <adurotimijoshua@gmail.com>
2025-04-22 10:37:54 +0100

    Added filter for transfereed tickets

--- src/app/rest/attendee/bookings/bookings.service.ts
+++ src/app/rest/attendee/bookings/bookings.service.ts
@@ -765,7 +765,7 @@ export class BookingsService {
 
     // unsure the event is not over yet
     const now = new Date();
-    const eventDate = new Date(booking.event.eventStartDateAndTime);
+    const eventDate = new Date(booking.event.eventEndDateAndTime);
     if (now > eventDate)
       throw new NotAcceptableException('Event has already ended');
 
